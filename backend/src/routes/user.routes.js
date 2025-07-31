eb00ee1281f72906afa06b9a37eb711337c16dec
Monish0210 <monishpatel831@gmail.com>
2025-07-31 22:44:21 +0530

    add email verification and profile update features

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, changeCurrentPassword, getCurrentUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser, changeCurrentPassword, getCurrentUser, refreshAccessToken, verifyEmail, updateUserDetails, updateAvatar, resendEmailVerificationLink } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -14,10 +14,23 @@ router.route("/register").post(
     ]),
     registerUser
 )
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/MyProfile").get(verifyJWT, getCurrentUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/MyProfile").get(verifyJWT, getCurrentUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/verify-email").get(verifyEmail);
+router.route("/update-details").patch(verifyJWT, updateUserDetails);
+router.route("/update-avatar").patch(
+    verifyJWT, 
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        }
+    ]),
+    updateAvatar
+);
+router.route("/resend-verification-link").post(resendEmailVerificationLink);
 
 export default router;
